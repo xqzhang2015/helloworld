8d58f7fae27a86ae55359de5043eac7d1279ad51
xqzhang <xqzhang@freewheel.tv>
2021-11-24 18:19:54 +0800

    go

--- go_singleflight/main.go
+++ go_singleflight/main.go
@@ -5,6 +5,9 @@ import (
 )
 
 // https://github.com/golang/go/blob/master/src/internal/singleflight/singleflight_test.go
+// https://github.com/golang/go/blob/master/src/net/lookup.go#L152
+// https://blog.csdn.net/weixin_31422487/article/details/112672038
+
 func main() {
 	fmt.Printf("hello world")
 }
